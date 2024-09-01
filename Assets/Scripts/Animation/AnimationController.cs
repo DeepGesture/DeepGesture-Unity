3ec02802d100ecc8740b2acc8e77ff605a888657
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-09-01 20:19:45 +0700

    intial update

---
+++ Assets/Scripts/Animation/AnimationController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using AI4Animation;
+
+[RequireComponent(typeof(Actor))]
+public abstract class AnimationController : MonoBehaviour {
+
+	public Actor Actor;
+	public float Framerate = 60f;
+
+	protected abstract void Setup();
+	protected abstract void Destroy();
+	protected abstract void Control();
+	protected abstract void OnGUIDerived();
+	protected abstract void OnRenderObjectDerived();
+
+	void Reset() {
+		// Debug.Log("Reset");
+		Actor = GetComponent<Actor>();
+	}
+
+    void Start() {
+	    Debug.Log("Start" + Framerate);
+		Time.fixedDeltaTime = 1f/Framerate;
+		Utility.SetFPS(Mathf.RoundToInt(Framerate));
+		Setup();
+    }
+
+	void OnDestroy() {
+		// Debug.Log("OnDestroy");
+		Destroy();
+	}
+
+	void FixedUpdate() {
+		// Debug.Log("FixedUpdate");
+		Control();
+	}
+
+    void OnGUI() {
+		OnGUIDerived();
+    }
+
+	void OnRenderObject() {
+		OnRenderObjectDerived();
+	}
+	
+}
