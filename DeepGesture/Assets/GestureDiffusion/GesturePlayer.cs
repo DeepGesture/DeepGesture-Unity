55fd067a3f40cf29517ff7d5b9b5064a494a0a25
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-10-30 00:40:45 +0700

    update GesturePlayer

--- DeepGesture/Assets/GestureDiffusion/GesturePlayer.cs
+++ DeepGesture/Assets/GestureDiffusion/GesturePlayer.cs
@@ -1,8 +1,11 @@
 # if UNITY_EDITOR
 using UnityEngine;
+using UnityEditor;
 using OpenHuman;
-using UltimateIK;
-using Unity.Barracuda;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DeepGesture {
 
@@ -12,17 +15,27 @@ namespace DeepGesture {
         private int[] BoneMapping = null;
         public float Framerate = 60f;
         public MotionAsset Asset;
-        private float Timestamp = 0f;
         private Actor Actor = null;
+        private float Timescale = 1f;
+        private float Timestamp = 0f;
 
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public float Zoom = 1f;
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        private EditorCoroutines.EditorCoroutine Coroutine = null;
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
         void Start() {
-            Debug.Log("GesturePlayer Start");
-            Actor = GetComponent<Actor>();
-
+            // Debug.Log("GesturePlayer Start");
+            Actor = GetComponentInChildren<Actor>();
         }
 
         void OnDestroy() {
+        }
+
+        public bool IsPlaying() {
+            return Coroutine != null;
         }
 
         public void InitializeDance(params object[] objects) {
@@ -37,38 +50,112 @@ namespace DeepGesture {
             Actor.SetBoneVelocities(asset.GetFrame(timestamp).GetBoneVelocities(Actor.GetBoneNames(), mirrored).DirectionsFromTo(reference, root, true));
         }
 
-        public void AnimateGesture(object[] parameters) {
+        public void Update() {
+            if (Application.isPlaying) {
+				LoadFrame(Mathf.Repeat(Timestamp + Time.deltaTime, Asset.Frames.Last().Timestamp));
+			}
         }
 
-        public void Update() {
-            // AudioSpectrum.PlayMusic(Timestamp, true);
+        public void Play(bool value) {
+            if (value && IsPlaying()) {
+                return;
+            }
+            if (!value && !IsPlaying()) {
+                return;
+            }
+
+            if (value) {
+                Coroutine = EditorCoroutines.StartCoroutine(OnPlay(), this);
+            }
+            else {
+                EditorCoroutines.StopCoroutine(OnPlay(), this);
+                Coroutine = null;
+            }
+        }
+
+        private IEnumerator OnPlay() {
+            DateTime previous = Utility.GetTimestamp();
+            while (Asset != null) {
+                float delta = Timescale * (float)Utility.GetElapsedTime(previous);
+                if (Timescale == 0f || delta > 1f / Framerate) {
+                    previous = Utility.GetTimestamp();
+                    LoadFrame(Mathf.Repeat(Timestamp + delta, Asset.Frames.Last().Timestamp));
+                }
+                yield return new WaitForSeconds(0f);
+            }
+        }
+
+        public Actor GetActor() {
+            return GetComponentInChildren<Actor>();
         }
 
         public void OnPlayWithParam(object[] parameters) {
             float timestamp = (float)parameters[1];
-            // Actor.DrawRoot = true;
-            Vector3 rootPosition = Actor.transform.position;
-            // Actor.transform
-            BoneMapping = Asset.Source.GetBoneIndices(Actor.GetBoneNames());
-            //Apply posture on character
-            for (int i = 0; i < Actor.Bones.Length; i++) {
+            LoadFrame(timestamp);
+            // Vector3 rootPosition = Actor.transform.position;
+            // BoneMapping = Asset.Source.GetBoneIndices(Actor.GetBoneNames());
+            // //Apply posture on character
+            // for (int i = 0; i < Actor.Bones.Length; i++) {
+            //     if (BoneMapping[i] != -1) {
+            //         Matrix4x4 boneMatrix = Asset.GetFrame(timestamp).GetBoneTransformation(BoneMapping[i], Mirror);
+            //         boneMatrix.m03 = boneMatrix.m03 + rootPosition.x;
+            //         boneMatrix.m13 = boneMatrix.m13 + rootPosition.y;
+            //         boneMatrix.m23 = boneMatrix.m23 + rootPosition.z;
+
+            //         Actor.Bones[i].SetTransformation(boneMatrix);
+            //         Actor.Bones[i].SetVelocity(
+            //             Asset.GetFrame(timestamp).GetBoneVelocity(BoneMapping[i], Mirror)
+            //         );
+            //         // Actor.Bones[i].SetAcceleration(
+            //         // 	Asset.GetFrame(Editor.GetTimestamp()).GetBoneAcceleration(BoneMapping[i], Mirror)
+            //         // );
+            //     }
+            // }
+        }
+
+        public void LoadFrame(float timestamp) {
+            if (Asset == null) {
+                return;
+            }
+
+            Timestamp = timestamp;
+            LoadFrame();
+        }
+
+        public void LoadFrame(){
+            Actor actor = GetActor();
+            Vector3 rootPosition = actor.transform.position;
+            BoneMapping = Asset.Source.GetBoneIndices(actor.GetBoneNames());
+
+            // Apply posture on character
+            for (int i = 0; i < actor.Bones.Length; i++) {
                 if (BoneMapping[i] != -1) {
-                    Matrix4x4 boneMatrix = Asset.GetFrame(timestamp).GetBoneTransformation(BoneMapping[i], Mirror);
-                    Matrix4x4 rootMatrix = Matrix4x4.TRS(rootPosition, Quaternion.identity, Vector3.zero);
-                    boneMatrix.TransformationFrom(rootMatrix);
-                    Actor.Bones[i].SetTransformation(
-                        boneMatrix
+                    Matrix4x4 boneMatrix = Asset.GetFrame(GetTimestamp()).GetBoneTransformation(BoneMapping[i], Mirror);
+                    boneMatrix.m03 = boneMatrix.m03 + rootPosition.x;
+                    boneMatrix.m13 = boneMatrix.m13 + rootPosition.y;
+                    boneMatrix.m23 = boneMatrix.m23 + rootPosition.z;
+
+                    actor.Bones[i].SetTransformation(boneMatrix);
+                    actor.Bones[i].SetVelocity(
+                        Asset.GetFrame(GetTimestamp()).GetBoneVelocity(BoneMapping[i], Mirror)
                     );
-                    Actor.Bones[i].SetVelocity(
-                        Asset.GetFrame(timestamp).GetBoneVelocity(BoneMapping[i], Mirror)
-                    );
-                    // Actor.Bones[i].SetAcceleration(
+                    // actor.Bones[i].SetAcceleration(
                     // 	Asset.GetFrame(Editor.GetTimestamp()).GetBoneAcceleration(BoneMapping[i], Mirror)
                     // );
                 }
             }
+
+            //Send callbacks to all modules
+            // Asset.Callback(e);
         }
 
+        public Frame GetCurrentFrame() {
+            return Asset.GetFrame(GetTimestamp());
+        }
+
+        public float GetTimestamp() {
+            return Timestamp;
+        }
 
         void OnGUI() {
         }
@@ -84,6 +171,57 @@ namespace DeepGesture {
     }
 
 
+
+    [CustomEditor(typeof(GesturePlayer))]
+    public class GesturePlayer_Editor : Editor {
+        public GesturePlayer Target;
+
+        void Awake(){
+            // Timestamp = Utility.GetTimestamp();
+            Target = (GesturePlayer)target;
+        }
+
+        public void OnEnable() {
+            Target = (GesturePlayer)target;
+        }
+
+        public override void OnInspectorGUI() {
+            Frame frame = Target.GetCurrentFrame();
+
+            Target.Asset = EditorGUILayout.ObjectField("Motion Asset", Target.Asset, typeof(MotionAsset), true) as MotionAsset;
+            Target.Mirror = EditorGUILayout.Toggle("Mirror", Target.Mirror);
+            Target.Framerate = EditorGUILayout.FloatField("Framerate", Target.Framerate);
+
+            using (new EditorGUILayout.VerticalScope("Box")) {
+                EditorGUILayout.BeginHorizontal();
+
+                GUILayout.FlexibleSpace();
+                if (Target.IsPlaying()) {
+                    if (Utility.GUIButton("⌧", Color.red, Color.white, 50f, 40f)) {
+                        Target.Play(false);
+                    }
+                }
+                else {
+                    if (Utility.GUIButton("▶", Color.green, Color.white, 50f, 40f)) {
+                        Target.Play(true);
+                    }
+                }
+
+                GUILayout.FlexibleSpace();
+
+                EditorGUILayout.BeginVertical();
+                int index = EditorGUILayout.IntSlider(frame.Index, 1, Target.Asset.GetTotalFrames());
+                if (index != frame.Index) {
+                    Target.LoadFrame(Target.Asset.GetFrame(index).Timestamp);
+                }
+                Target.Zoom = EditorGUILayout.Slider(Target.Zoom, 0f, 1f);
+                EditorGUILayout.EndVertical();
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+        }
+    }
 }
 
 #endif
